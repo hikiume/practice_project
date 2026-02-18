dace6829bcac85475ac8e93d245f9d8ba50122da
Hiki <hikasa@hikasanoMacBook-Air.local>
2026-02-18 19:50:44 +0900

    [add] todo_v3.rs use for and loop

---
+++ src/bin/todo_v3.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let mut tasks: Vec<Task> = Vec::new();
+
+    loop {
+        let mut input = String::new();
+
+        std::io::stdin().read_line(&mut input).expect("エラー");
+
+        if input.trim() == "quit" {
+            break;
+        }
+
+        tasks.push(Task {
+            name: (input.trim().to_string()),
+            compoleted: (false),
+        });
+    }
+
+    for task in &tasks{
+        println!("name : {} completed : {}",task.name,task.compoleted)
+    }
+    
+}
+
+struct Task {
+    name: String,
+    compoleted: bool,
+}
