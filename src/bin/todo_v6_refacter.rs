fcad3af0a1a5fe5ef2b657b9959097d5f4c67723
Hiki <hikasa@hikasanoMacBook-Air.local>
2026-02-21 14:58:26 +0900

    [add] todo_v6_refacter

---
+++ src/bin/todo_v6_refacter.rs
@@ -0,0 +1,48 @@
+use std::{
+    fs::File,
+    io::{Read, Write, stdin},
+};
+
+use serde::{Deserialize, Serialize};
+
+fn main() {
+    let mut content = String::new();
+    let mut file = File::open("tasks.json").expect("エラー");
+    file.read_to_string(&mut content).expect("エラー");
+
+    let mut tasks: Vec<Task> = serde_json::from_str(&content).expect("エラー");
+
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).expect("エラー");
+
+        match input.trim() {
+            "quit" => break,
+            "all complete" => {
+                tasks.iter_mut().for_each(|v| v.completed = true);
+                break;
+            },
+            _ =>(),
+        }
+
+        tasks.push(Task {
+            name: (input.trim().to_string()),
+            completed: (false),
+        });
+    }
+
+    tasks
+        .iter()
+        .for_each(|v| println!("v.name :{} v.conpleted :{}", v.name, v.completed));
+
+    let json = serde_json::to_string_pretty(&tasks).expect("エラー");
+
+    let mut write_file = File::create("tasks.json").expect("エラー");
+    write_file.write_all(json.as_bytes()).expect("エラー");
+}
+
+#[derive(Serialize, Deserialize)]
+struct Task {
+    name: String,
+    completed: bool,
+}
